838343073e296b1b71684ba9e6813a7efac8fe33
Dallin Stewart <dallinpstewart@gmail.com>
2021-09-28 16:57:14 -0600

    fix unsigned int

--- Parser.cpp
+++ Parser.cpp
@@ -118,7 +118,7 @@ void Parser::parseFact() {
         parseStringList(newParameters);
         parseToken(tokens[index], RIGHT_PAREN);
         parseToken(tokens[index], PERIOD);
-        int factLength = index - tempIndex + 1;
+        unsigned int factLength = index - tempIndex + 1;
         for (unsigned int i=0; i<factLength; i++) {
             tempFact += tokens[i+tempIndex]->getValue();
         }
